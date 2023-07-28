4f7867413936f8a0f8e900876b9c6ea53e2bb50d
Roman Chenoweth <romanc22@vt.edu>
2023-07-28 17:55:04 +0000

    checking

---
+++ front_end/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Sample Game component
+function Game() {
+  return (
+    <div>
+      <h1>Welcome to My Game!</h1>
+      {/* Add the rest of your game components and logic here */}
+    </div>
+  );
+}
+
+// Rendering the Game component
+ReactDOM.render(<Game />, document.getElementById("root"));
