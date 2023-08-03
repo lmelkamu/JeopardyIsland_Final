924b60ba82fe34b39538fcefceeabdb3b4651d3c
lmelkamu <lydiamelkamu24@gmail.com>
2023-08-03 21:14:52 +0000

    done for the day

--- lib/jeopardy_graphics.ml
+++ lib/jeopardy_graphics.ml
@@ -45,8 +45,8 @@ let init_exn () =
   Graphics.open_graph
     (Printf.sprintf
        " %dx%d"
-       (play_area_height + header_height)
-       play_area_width);
+       play_area_width
+       (play_area_height + header_height));
   Game.create Game.Level.T.Easy
 ;;
 
@@ -131,14 +131,10 @@ let draw_board (game : Game.t) =
   draw_islands game;
   (* box 2: top header *)
   Graphics.set_color Colors.head_color;
-  Graphics.fill_rect
-    0
-    (play_area_height - header_height)
-    play_area_width
-    header_height;
-  Graphics.moveto (play_area_width / 2) 80;
-  Graphics.set_color Colors.red;
+  Graphics.fill_rect 0 play_area_height play_area_width header_height;
+  Graphics.moveto (play_area_width / 2) (play_area_height + 20);
   let header_text = Game.Game_state.to_string game_state in
+  Graphics.set_color Colors.black;
   Graphics.draw_string (Printf.sprintf " %s" header_text);
   Graphics.moveto (play_area_width - 40) (play_area_height - 20);
   Graphics.draw_string (Printf.sprintf "Player_2 Score: %d" player_two_score);
@@ -146,6 +142,7 @@ let draw_board (game : Game.t) =
   Graphics.draw_string (Printf.sprintf "Player_1 Score: %d" player_one_score);
   Graphics.set_color Colors.head_color;
   (* box 3: bottom box *)
+  Graphics.set_color Colors.head_color;
   Graphics.fill_rect 0 0 play_area_width header_height;
   (* Graphics.moveto right_shift (play_area_height - 70);
      Graphics.draw_string "A:"; Graphics.moveto ((play_area_width / 4) +
